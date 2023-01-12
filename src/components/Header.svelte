205b696bfee455c09b949698a60e02094b111cbf
one-tokyo <takumi@fogca.jp>
2023-01-13 02:34:21 +0900

    0113_0230

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,13 +1,45 @@
+<svelte:window on:scroll={scrollToWorks} />
 <script>
     import { slide } from 'svelte/transition';
     import Saos from "saos";
     import Logo from './Logo.svelte';
     import LogoA from './Logo@.svelte';
+    import { browser } from '$app/environment';
 
     let clicked = false;
 	let isExpanded = false
 	function clickHandler() {
 		isExpanded = !isExpanded  
+    }
+
+    function scrollToWorks() {
+        if (browser) {
+            let header = document.getElementsByTagName('header');
+            let currentY = window.pageYOffset;
+            let winHeight = window.innerHeight;
+            //let set_position = 0;
+            window.addEventListener("scroll", () => {
+                if ( currentY < winHeight * .5 ) {/* 50vh以下 */
+                    header[0].classList.add('top');
+                } else if ( currentY > winHeight * .5 && currentY < winHeight * 1.5 ) {/* 50vh以上 && 150vh以下*/
+                    header[0].classList.add('show');
+                    header[0].classList.remove('top');
+                } else if (currentY > winHeight * 1.5 ) {/* 150vh以上 */
+                    header[0].classList.remove('show');
+                };
+
+                if (currentY < document.documentElement.scrollTop) {
+                    header[0].classList.add('down');
+                    header[0].classList.remove('up');
+                } else {
+                    header[0].classList.add('up');
+                    header[0].classList.remove('down');
+                }
+                //set_position = document.documentElement.scrollTop;
+                
+            });
+        }
+        
     }
 	
 </script>
@@ -28,9 +60,7 @@
 <style>
 
 header {
-
-    opacity: 1;
-
+    opacity: 0;
     width: 5rem;
     height: 100vh;
     position: fixed;
@@ -47,7 +77,9 @@ header {
     align-items: center;
     justify-content: space-between;
     padding: 3rem 0 5rem;
+    transition: .5s ease;
 }
+header.show {opacity: 1;}
 
 header .top {width: 72.5%;}
 header .top ul {margin-top: 4rem;}
